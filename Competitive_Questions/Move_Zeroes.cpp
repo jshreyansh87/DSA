5fef4892f2a7d4d13599953649546303dc7e35f4
Shreyansh Jain <jshreyansh87@gmail.com>
2022-06-06 17:20:21 +0530

    Solved Merge Zeroes

---
+++ Competitive_Questions/Move_Zeroes.cpp
@@ -0,0 +1,17 @@
+// Problem Statement => https://leetcode.com/problems/move-zeroes/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for(int j = 0; j < nums.size(); j++){
+            if(nums[j] != 0){
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+    }
+};
