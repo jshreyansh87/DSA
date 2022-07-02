478b7e3f0f7848f31d564f9d753f3ee0f60487c6
Shreyansh Jain <jshreyansh87@gmail.com>
2022-07-02 14:55:30 +0530

    Solved Reverse Stack Using Recursion

---
+++ Competitive_Questions/Reverse_Stack_Using_Recursion.cpp
@@ -0,0 +1,35 @@
+// Problem Statement => https://www.codingninjas.com/codestudio/problems/reverse-stack-using-recursion_631875
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertAtBottom(stack<int> &s, int element) {
+    //base case
+    if(s.empty()) {
+        s.push(element);
+          return ;
+    }
+    
+    int num = s.top();
+    s.pop();
+    
+    //recursive call
+    insertAtBottom(s, element);
+    
+    s.push(num);
+}
+
+void reverseStack(stack<int> &stack) {
+      //base case
+    if(stack.empty()) {
+        return ;
+    }
+    
+    int num = stack.top();
+    stack.pop();
+    
+    //recursive call
+    reverseStack(stack);
+    
+    insertAtBottom(stack,num);
+}
